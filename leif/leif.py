ef3d8d9a1d87592405c174fdc4f34397fe0ae92a
Christian Geier <geier@lostpackets.de>
2014-03-11 00:27:58 +0100

    some refactoring

--- leif/leif.py
+++ leif/leif.py
@@ -4,12 +4,35 @@ import requests
 import lxml.etree as etree
 
 
-CALENDAR = 'calendar'
-ABOOK = 'addressbook'
-
-
 class Discover(object):
+
+    xml_home = ''
+    xml_collection = ''
+
+    str_homeset = ''
+
     def __init__(self, url, user=None, password=None, ssl_verify=True):
+        """
+        :param url: the base url from which to start discovery, if no scheme
+                    ('http://' or 'https://') is given, https will be used
+        :type url: unicode
+        :param user: the username, if a username is part of the url,
+                     this one is discarded
+        :type user: unicode
+        :param password: password used for authentication, if a password
+                         is part of the url, this one is discarded
+        :type password: unicode
+        :param ssl_verify: how to verify SSL/TLS connections, passed through
+                           to requests,
+                           * if True will do normal certificate verification
+                             with the default CA bundle,
+                           * if set to a string it will try to import the file
+                             at the string's location as a CA bundle,
+                           * if set to False no certificate verification
+                             whatsoever will be done
+        :type ssl_verify: unicode/bool
+        """
+
         if urlparse.urlsplit(url).scheme == '':
             url = 'https://' + url
         self.starturl = url
@@ -26,6 +49,11 @@ class Discover(object):
         self._default_headers = {'User-Agent': 'leif 0.1'}
 
     def _find_principal(self):
+        """tries to find the principal URL of the user
+        :returns: iterable (but should be only of element) of urls
+        :rtype: iterable(unicode)
+
+        """
         headers = dict()
         headers.update(self._default_headers)
         headers['Depth'] = 0
@@ -45,6 +73,10 @@ class Discover(object):
                     yield principal.text
 
     def discover(self):
+        """discover all the user's CalDAV or CardDAV collections on the server
+        :returns: a list of the user's collections (as urls)
+        :rtype: list(unicode)
+        """
         collections = list()
         for principal in self._find_principal():
             for home in self._find_home(principal):
@@ -53,48 +85,49 @@ class Discover(object):
         return collections
 
     def _find_home(self, principal):
-        raise NotImplementedError()
-
-    def _find_collections(self, home):
-        raise NotImplementedError()
-
-class CalDiscover(Discover):
-
-    def _find_home(self, principal):
         headers = dict()
         headers.update(self._default_headers)
         headers['Depth'] = 0
         url = self.baseurl + principal
-        body = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
-  <d:prop>
-    <c:calendar-home-set />
-  </d:prop>
-</d:propfind>"""
         res = requests.request('PROPFIND', url, auth=self.auth,
-                               headers=headers, data=body)
+                               headers=headers, data=self.xml_home)
         res.raise_for_status()
         root = etree.fromstring(res.text.encode('utf-8'))
 
-        for element in root.iter('{*}calendar-home-set'):
+        for element in root.iter(self.str_homeset):
             for homeset in element.iter():
                 if homeset.tag.endswith('href'):
                     yield homeset.text
 
     def _find_collections(self, home):
-        """find all CalDAV and CardDAV collections under `home`"""
-        headers = dict()
-        headers.update(self._default_headers)
-        headers['Depth'] = 1
-        url = self.baseurl + home
-        body = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
+        raise NotImplementedError()
+
+
+class CalDiscover(Discover):
+
+    xml_home = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
+  <d:prop>
+    <c:calendar-home-set />
+  </d:prop>
+</d:propfind>"""
+    xml_collection = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
   <d:prop>
      <d:resourcetype />
      <d:displayname />
      <c:supported-calendar-component-set />
   </d:prop>
 </d:propfind>"""
+    str_homeset = '{*}calendar-home-set'
+
+    def _find_collections(self, home):
+        """find all CalDAV collections under `home`"""
+
+        headers = dict()
+        headers.update(self._default_headers)
+        headers['Depth'] = 1
+        url = self.baseurl + home
         res = requests.request('PROPFIND', url, auth=self.auth,
-                               headers=headers, data=body)
+                               headers=headers, data=self.xml_collection)
         res.raise_for_status()
         root = etree.fromstring(res.text.encode('utf-8'))
         for response in root.iter('{*}response'):
@@ -107,7 +140,7 @@ class CalDiscover(Discover):
                 calendar['displayname'] = ''
 
             if prop.find('{*}resourcetype/{*}calendar') is not None:
-                calendar['type'] = CALENDAR
+                calendar['type'] = 'calendar'
             else:
                 calendar['type'] = None
 
@@ -122,41 +155,27 @@ class CalDiscover(Discover):
 
 
 class CardDiscover(Discover):
-
-    def _find_home(self, principal):
-        headers = dict()
-        headers.update(self._default_headers)
-        headers['Depth'] = 0
-        url = self.baseurl + principal
-        body = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:carddav">
+    xml_home = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:carddav">
   <d:prop>
     <c:addressbook-home-set />
   </d:prop>
 </d:propfind>"""
-        res = requests.request('PROPFIND', url, auth=self.auth,
-                               headers=headers, data=body)
-        res.raise_for_status()
-        root = etree.fromstring(res.text.encode('utf-8'))
-
-        for element in root.iter('{*}addressbook-home-set'):
-            for homeset in element.iter():
-                if homeset.tag.endswith('href'):
-                    yield homeset.text
-
-    def _find_collections(self, home):
-        """find all CalDAV and CardDAV collections under `home`"""
-        headers = dict()
-        headers.update(self._default_headers)
-        headers['Depth'] = 1
-        url = self.baseurl + home
-        body = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:cardav">
+    xml_collection = """<d:propfind xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:cardav">
   <d:prop>
      <d:resourcetype />
      <c:addressbook />
   </d:prop>
 </d:propfind>"""
+    str_homeset = '{*}addressbook-home-set'
+
+    def _find_collections(self, home):
+        """find all CardDAV collections under `home`"""
+        headers = dict()
+        headers.update(self._default_headers)
+        headers['Depth'] = 1
+        url = self.baseurl + home
         res = requests.request('PROPFIND', url, auth=self.auth,
-                               headers=headers, data=body)
+                               headers=headers, data=self.xml_collection)
         res.raise_for_status()
         root = etree.fromstring(res.text.encode('utf-8'))
         for response in root.iter('{*}response'):
@@ -171,12 +190,11 @@ class CardDiscover(Discover):
                 yield calendar
 
 
-
 if __name__ == "__main__":
     import getpass
     import sys
     url = raw_input('URL: ')
-    user = raw_input('username: ')
+    user = raw_input('Username: ')
     password = getpass.getpass('Password: ')
     try:
         discoverer = CalDiscover(url, user, password)
@@ -189,18 +207,18 @@ if __name__ == "__main__":
         ssl_verify = raw_input("path to SSL cert bundle (see notes): ")
         if ssl_verify == 'False':
             ssl_verify = False
-            cont = raw_input("SSL verification disabled, are you sure you want to continue?[y/N] ")
+            cont = raw_input("SSL verification disabled, are you sure you"
+                             "want to continue?[y/N] ")
             if cont != 'y':
                 sys.exit("aborting...")
 
         discoverer = CalDiscover(url, user, password, ssl_verify)
         calendars = discoverer.discover()
         discoverer = CardDiscover(url, user, password, ssl_verify)
-        abooks= discoverer.discover()
+        abooks = discoverer.discover()
 
     if calendars == list() and abooks == list():
         sys.exit("Found nothing, sorry...")
-
 
     for collections, name in [(calendars, 'CalDAV'), (abooks, 'CardDAV')]:
         if collections == list():
